2eccd5540c278e213ffea1ae22ca1bbc0bec00b8
Syed Mohammad Ibrahim <iamibi@umd.edu>
2022-02-12 17:37:19 -0500

    Added test script

--- Homework/Homework - 1/Test Homework - 1.py
+++ Homework/Homework - 1/Test Homework - 1.py
@@ -1,41 +1,4 @@
-# My Information
-UID = 118428369
-Last_Name = "Ibrahim"
-First_Name = "Syed Mohammad"
-
-
-def caesar_str_enc(plain_text: str, encryption_key: int) -> str:
-    """ 
-        A Caesar Cipher encryption function which takes in a
-        plain text string and an integer key and returns a ciphered string.
-    """
-    
-    # Make string uppercase
-    updated_text = plain_text.upper()
-    
-    # Remove the spaces and escape characters from the string
-    updated_text = updated_text.strip().replace(' ', '')
-
-    # ASCII value of 'A'
-    ascii_val = 65
-    cipher_text = []
-    for char in updated_text:
-        cipher_char = char
-        if char.isalpha():
-            cipher_char = chr((ord(char) + encryption_key + ascii_val) % 26 + ascii_val)
-        cipher_text.append(cipher_char)
-
-    return ''.join(cipher_text)
-    
-
-def caesar_str_dec(cipher_text: str, decryption_key: int) -> str:
-    """ 
-        A Caesar Cipher decryption function which takes in a
-        cipher text string and an integer key and returns the plain text string.
-    """
-
-    return caesar_str_enc(cipher_text, 26 - (decryption_key % 26))
-
+from Homework_1_Solution import caesar_str_enc, caesar_str_dec
 
 if __name__ == '__main__':
     original_plain_str = 'hi 123 abc parse it  '
